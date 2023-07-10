c57936516cad69ac6dfc868e6da63a9061e84012
Robert Orlikowski <38508936+robert72127@users.noreply.github.com>
2023-07-10 22:56:15 +0200

    large file via double indirect

--- kernel/fs.c
+++ kernel/fs.c
@@ -383,7 +383,7 @@ static uint
 bmap(struct inode *ip, uint bn)
 {
   uint addr, *a;
-  struct buf *bp;
+  struct buf *bp, *bpd;
 
   if(bn < NDIRECT){
     if((addr = ip->addrs[bn]) == 0){
@@ -416,6 +416,38 @@ bmap(struct inode *ip, uint bn)
     brelse(bp);
     return addr;
   }
+  bn -= NINDIRECT;
+
+  if(bn < NDBLINDIRECT){
+    // Load indirect block, allocating if necessary.
+    if((addr = ip->addrs[NDIRECT+1]) == 0){
+      addr = balloc(ip->dev);
+      if(addr == 0)
+        return 0;
+      ip->addrs[NDIRECT+1] = addr;
+    }
+    bp = bread(ip->dev, addr); //first level indirect block
+    a = (uint*)bp->data;
+    if((addr = a[bn/NINDIRECT]) == 0 ){ //pick correct second level indirect
+      addr = balloc(ip->dev);
+      if(addr == 0)
+        return 0;
+      a[bn/NINDIRECT] = addr;
+    }
+    bpd = bread(ip->dev, addr); // second level indirect
+    a = (uint*)bpd->data;
+    if((addr = a[bn % NINDIRECT]) == 0){
+      addr = balloc(ip->dev);
+      if(addr){
+        a[bn % NINDIRECT]  = addr;
+        log_write(bpd);
+        log_write(bp);
+      }
+    }
+    brelse(bpd);
+    brelse(bp);
+    return addr;
+  }
 
   panic("bmap: out of range");
 }
@@ -425,9 +457,9 @@ bmap(struct inode *ip, uint bn)
 void
 itrunc(struct inode *ip)
 {
-  int i, j;
-  struct buf *bp;
-  uint *a;
+  int i, j, k;
+  struct buf *bp, *bpd;
+  uint  *a, *b;
 
   for(i = 0; i < NDIRECT; i++){
     if(ip->addrs[i]){
@@ -447,6 +479,34 @@ itrunc(struct inode *ip)
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
   }
+
+  if(ip->addrs[NDIRECT+1]){
+
+    bp = bread(ip->dev, ip->addrs[NDIRECT+1]);
+    a = (uint *)bp->data;
+    for(j = 0; j < NINDIRECT; j++){
+        if(a[j]){
+
+          bpd = bread(ip->dev, a[j]); //read correct second level indirect
+
+          b = (uint*)bpd->data;
+          for(k = 0; k< NINDIRECT; k ++){
+            if(b[k])  
+              bfree(ip->dev, b[k]);
+          }
+
+          brelse(bpd);
+          bfree(ip->dev, a[j]);
+        
+        }
+
+    }
+    brelse(bp);
+    bfree(ip->dev, ip->addrs[NDIRECT+1]);
+    ip->addrs[NDIRECT+1] = 0;
+
+  }
+
 
   ip->size = 0;
   iupdate(ip);
